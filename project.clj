03b42e7db4e34f76acbd1a9efc5059df342c617d
Anton Logvinenko <logvinenko@farpost.com>
2014-01-06 15:30:07 +1100

    Faces detection

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-localrepo "0.5.3"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [enlive "1.1.1"]])
+                 [enlive "1.1.1"]
+                 [opencv/opencv "3.0.0"]
+                 [opencv/opencv-native "3.0.0"]])
--- src/wild_party_face_recognition/detect.clj
+++ src/wild_party_face_recognition/detect.clj
@@ -1,1 +1,45 @@
-(ns wild-party-face-recognition.detect)
+(ns wild-party-face-recognition.detect
+  (:import
+   org.opencv.core.Core
+   org.opencv.core.Mat
+   org.opencv.core.MatOfRect
+   org.opencv.core.Point
+   org.opencv.core.Rect
+   org.opencv.core.Scalar
+   org.opencv.highgui.Highgui
+   org.opencv.objdetect.CascadeClassifier))
+
+(def face-detections (atom []))
+
+(defn create-classifier []
+  (-> "lbpcascade_frontalface.xml" clojure.java.io/resource .getPath CascadeClassifier.))
+
+(defn load-image [path]
+  (Highgui/imread path))
+
+(defn detect-faces! [classifier image]
+  (.detectMultiScale classifier image @face-detections))
+
+(defn draw-bounding-boxes! [image to-file]
+  (doall (map (fn [rect]
+                (Core/rectangle image
+                                (Point. (.x rect) (.y rect))
+                                (Point. (+ (.x rect) (.width rect))
+                                        (+ (.y rect) (.height rect)))
+                                (Scalar. 0 255 0)))
+              (.toArray @face-detections)))
+  (Highgui/imwrite to-file image))
+
+(defn process-and-save-image! [in out]
+  (let [image (load-image in)]
+    (detect-faces! (create-classifier) image)
+    (draw-bounding-boxes! image out)))
+
+(defn find [name]
+  (let [path "/Users/anton/Desktop/"]
+    (main (str path "dest/" name ".jpg") (str path "out/" name ".png"))))
+
+(defn main [fin fout]
+  (clojure.lang.RT/loadLibrary Core/NATIVE_LIBRARY_NAME)
+  (reset! face-detections (MatOfRect.))
+  (process-and-save-image! fin fout))
